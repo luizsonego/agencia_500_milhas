8790d46e2b694d3eb934b28a8b69b5fb77fe11a4
luiz <leesonego@gmail.com>
2021-06-22 15:32:31 -0300

    added hotjar

--- pages/_document.js
+++ pages/_document.js
@@ -19,6 +19,23 @@ export default class MyDocument extends Document {
             `,
             }}
           />
+
+          {/* <!-- Hotjar Tracking Code for https://agencia.500milhas.com.br/ --> */}   
+            <script 
+              dangerouslySetInnerHTML={{ 
+                __html: `
+                (function(h,o,t,j,a,r){
+                  h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+                      h._hjSettings={hjid:2467514,hjsv:6};
+                      a=o.getElementsByTagName('head')[0];
+                      r=o.createElement('script');r.async=1;
+                      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                      a.appendChild(r);
+                  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                `
+              }}
+            />
+          
         </Head>
         <body>
           <Main />
